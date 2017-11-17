0e8f9dd4a394eddd55c517cf9e111a9fe2739c41
Andre Rocha <andre.rocha@cern.ch>
2017-11-17 22:12:54 +0100

    Added modal for book details

--- src/Book.js
+++ src/Book.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import Modal from 'simple-react-modal'
 
 /**
  * Created by apaivaer on 04/11/2017.
@@ -18,17 +19,35 @@ class Book extends Component {
         updateBookShelf: PropTypes.func.isRequired
     }
 
+    state = {
+        show: false,
+    }
+
+    showBookModal = ()=>{
+        this.setState({show: true})
+    }
+
+    closeBookModal = ()=>{
+        this.setState({show: false})
+    }
+
     render() {
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover"
-                         style={{
-                             width: 128,
-                             height: 193,
-                             backgroundImage: `url(${this.props.book.imageLinks.thumbnail ? this.props.book.imageLinks.thumbnail : ""})`,
-                             borderStyle: `${this.props.book.shelf === 'none' ? 'none' : 'solid'}`
-                         }}>
+                    <div className="tooltip">
+                        <div className="book-cover"
+                             onClick={this.showBookModal}
+                             style={{
+                                 width: 128,
+                                 height: 193,
+                                 backgroundImage: `url(${this.props.book.imageLinks.thumbnail ? this.props.book.imageLinks.thumbnail : ""})`,
+                                 borderStyle: `${this.props.book.shelf === 'none' ? 'none' : 'solid'}`
+                             }}>
+                             <span className="tooltiptext" onClick={this.showBookModal}>
+                            Click to see details
+                        </span>
+                        </div>
                     </div>
                     <div className="book-shelf-changer">
                         <select defaultValue={this.props.book.shelf}
@@ -45,6 +64,16 @@ class Book extends Component {
                 </div>
                 <div className="book-title">{this.props.book.title}</div>
                 <div className="book-authors">{this.props.book.authors}</div>
+                <Modal show={this.state.show}
+                       onClose={this.closeBookModal}
+                       containerStyle={{width: '600px'}}
+                >
+                    <h2>{this.props.book.title}</h2>
+                    <span>By {this.props.book.authors}, {this.props.book.pageCount} pages</span>
+                    <br/>Publisher: {this.props.book.publisher}
+                    <p>Description: {this.props.book.description}</p>
+                </Modal>
+
             </div>
         )
     }
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,12 +1,22 @@
-/**
- * Created by apaivaer on 04/11/2017.
- */
-
 import React, { Component } from 'react'
 import * as BooksAPI from './BooksAPI'
 import Book from './Book'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+
+/**
+ * Created by apaivaer on 04/11/2017.
+ * SearhBooks component: This component allows to search of books
+ * and to add books to shelves. For a book to be displayed, it must
+ * have a title, an author, and an image.
+ *
+ * @Props:
+ *  - allBooksTracked: array of all books already in shelves
+ *  - updateBookShelf: function to add book to a shelf
+ *
+ * @State:
+ *  - books: array of books to be displayed
+ */
 
 class SearchBooks extends Component {
 
@@ -26,6 +36,7 @@ class SearchBooks extends Component {
             //Trim
             let userQuery = e.target.value.trim()
 
+            //API call and response processing
             BooksAPI.search(userQuery, 20).then((responseBooks) => {
                 if(!responseBooks.hasOwnProperty("error")) {
                     //Protection against null values in the backend
@@ -36,7 +47,7 @@ class SearchBooks extends Component {
                     //Get IDs of tracked books to array
                     let idsOfTrackedBooks = this.props.allBooksTracked.map((book)=> book.id)
 
-                    //Check if any of the books are already being tracked
+                    //Check if any of the books in the response are already being tracked
                     responseBooksFiltered = responseBooksFiltered.map((book)=> {
                         if(idsOfTrackedBooks.indexOf(book.id) >= 0) {
                             //Add a shelf to the book already tracked so that the menu displays correctly
@@ -68,16 +79,7 @@ class SearchBooks extends Component {
                 <div className="search-books-bar">
                     <Link to="/" className="close-search">Close</Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                         NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                         You can find these search terms here:
-                         https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                         However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                         you don't find a specific author or title. Every search is limited by search terms.
-                         */}
                         <input type="text" placeholder="Search by title or author" onChange={this.processQuery}/>
-
                     </div>
                 </div>
                 <div className="search-books-results">
